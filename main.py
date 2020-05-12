c710918fcd25b51daa80276290737b164143dff2
장한빈 <janghanbin@janghanbin-ui-MacBook-Pro.local>
2020-05-12 12:50:56 +0900

    add skel bootstrap code

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask import Flask,jsonify,abort,make_response,json, render_template, request, flash, redirect, url_for
 from flask_bootstrap import Bootstrap
 from attack_search import Attack
+import json
 app = Flask(__name__)
 Bootstrap(app)
 
+#
 @app.route('/')
-def root():
+def index():
     return render_template('index.html')
 
 @app.route('/api', methods=['GET', 'POST'])
@@ -14,7 +16,16 @@ def api():
     attack = Attack()
     attack_json = attack.get_bundle_json(keyword)
 
-    return attack_json
+    return json.dumps(json.loads(attack_json),sort_keys=True, indent=4)
+
+
+@app.route('/group', methods=['GET', 'POST'])
+def grouping():
+    return 'group'
+
+@app.route('/killchain', methods=['GET', 'POST'])
+def killchain():
+    return 'killchain'
 
 if __name__=='__main__':
     app.config.from_object(__name__)
